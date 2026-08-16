d107757a9db5f6a6366626ad20e8b6bd6c901f32
BraveCactus <mattdav961@gmail.com>
2026-08-16 12:42:06 +0300

    update config.py: add mlflow dependencies

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ class Config:
 
     eda_condition: bool = False
 
+    mlflow_tracking_uri: str = "http://127.0.0.1:5000"
+    experiment_name: str = "Ames_Housing_Experiments"
+
     def __post_init__(self):
         self.train_data_path = self.dataset_path / "train.csv"
         self.test_data_path = self.dataset_path / "test.csv"
